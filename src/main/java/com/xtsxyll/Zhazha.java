b233d001c003cdc3e8a9df8bf3892a79949f8cd3
Ling-G <xyll@xtsxyll.com>
2020-12-01 11:52:44 +0800

    init

---
+++ src/main/java/com/xtsxyll/Zhazha.java
@@ -0,0 +1,17 @@
+package com.xtsxyll;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Zhazha extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
